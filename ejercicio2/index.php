77997482b9bf80daf9a6931e1f2e069018bc56b9
EdisonStalin <edison.jumbo@epn.edu.ec>
2019-04-16 19:49:17 -0500

    Ejercicio2

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -1,7 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio2</title>
+</head>
+<body>
 <?php
 /**
  * Problema propuesto:
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
-*/
+	
+**/
+function Imprimir($titulo) {
+  
+  echo "<h1 style=\"text-align:center\">";
+  echo $titulo;
+
+  }
+  	echo "<br>";
+  	echo "<br>";
+	imprimir ("HOLA");
+	echo "<br>";
+	echo "<br>";
+	imprimir ("MUNDO!!!");
 ?>
+</body>
+</html>
+
+
+
+
+
+
